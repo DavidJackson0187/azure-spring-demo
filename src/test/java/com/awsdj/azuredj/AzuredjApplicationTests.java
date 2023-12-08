41097d01346a24070fb4daa9949dc247ae640f7b
DavidJackson0187 <davidjackson.0187@gmail.com>
2023-12-08 16:55:04 -0500

    first commit

---
+++ src/test/java/com/awsdj/azuredj/AzuredjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsdj.azuredj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuredjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
